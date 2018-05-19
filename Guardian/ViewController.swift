3996f1c3b90ead71dddef3589a1d6154cdb26072
EdwardChen <309156400@qq.com>
2018-05-20 00:58:25 +1000

    Commit

--- Guardian/ViewController.swift
+++ Guardian/ViewController.swift
@@ -7,14 +7,38 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,MKMapViewDelegate,CLLocationManagerDelegate {
+    @IBOutlet weak var mapKitView: MKMapView!
+    let locationManager = CLLocationManager()
+    
+    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
+        let location = locations.last as! CLLocation
+        
+        let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+        print(center)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+      
+        mapKitView.delegate = self
+        mapKitView.showsScale = true
+        mapKitView.showsPointsOfInterest = true
+        mapKitView.showsUserLocation = true
+        locationManager.requestAlwaysAuthorization()
+        locationManager.requestWhenInUseAuthorization()
+        
+        if CLLocationManager.locationServicesEnabled(){
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.startUpdatingLocation()
+        }
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
